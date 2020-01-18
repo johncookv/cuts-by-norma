93d3d2e3f76b342ef5d7012659a1191f10978b45
johncookv <john.vehikite@plastiq.com>
2020-01-18 08:44:48 -0800

    Remove logs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,8 @@
 const path = require('path')
 
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-console.log('process.env.GATSBY_SHOP_NAME: ', process.env.GATSBY_SHOP_NAME)
-console.log(
-  'process.env.GATSBY_SHOPIFY_ACCESS_TOKEN: ',
-  process.env.GATSBY_SHOPIFY_ACCESS_TOKEN
-)
 
 module.exports = {
   siteMetadata: {
