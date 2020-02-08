14422bb73dd0e5d3d45b8700713d3fe79c4bc198
johncookv <john.vehikite@plastiq.com>
2020-02-08 00:00:17 -0800

    Update homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import ProductGrid from '~/components/ProductGrid'
 const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Cuts by Norma</h1>
-    <p>Where shirts become art! Design update coming soon!</p>
+    <h1>Cuts by Norma - where shirts become art!</h1>
+    <h3>*Free Shipping Over $75!*</h3>
     <ProductGrid />
   </>
 )
