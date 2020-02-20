3e7865e756ff09a329cc121a9b85cf43b64a9ac2
johncookv <john.vehikite@plastiq.com>
2020-02-20 14:56:15 -0800

    Fix free shipping copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Cuts by Norma - where shirts become art!</h1>
-    <h3>*Free Shipping Over $75!*</h3>
+    <h3>*Free Shipping on orders $75 or more!*</h3>
     <ProductGrid />
   </>
 )
